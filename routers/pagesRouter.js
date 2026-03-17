02a0cdc328d741d1b66083179fbee6ec978fbd23
Nilket <joni0010@stud.kea.dk>
2026-03-17 22:36:23 +0100

    Final product, goodnight

--- routers/pagesRouter.js
+++ routers/pagesRouter.js
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 
-// import { frontpagePage, week1Page, week2Page, week3Page, week4Page, week5Page, week6Page, week7Page } from '../util/pagesUtil.js';
-
-import {builtPages} from '../util/pagesUtil.js';
+import { builtPages } from '../util/pagesUtil.js';
 
 const router = Router();
 
@@ -15,33 +13,3 @@ export default router;
 // router.get('/', (req, res) => {
 //     res.send(frontpagePage);
 // });
-
-// router.get('/week1/weekone.html', (req, res) =>{
-//     res.send(week1Page)
-// });
-
-// router.get('/week2/weektwo.html', (req, res) =>{
-//     res.send(week2Page)
-// });
-
-// router.get('/week3/weekthree.html', (req, res) =>{
-//     res.send(week3Page)
-// });
-
-// router.get('/week4/weekfour.html', (req, res) =>{
-//     res.send(week4Page)
-// });
-
-// router.get('/week5/weekfive.html', (req, res) =>{
-//     res.send(week5Page)
-// });
-
-// router.get('/week6/weeksix.html', (req, res) =>{
-//     res.send(week6Page)
-// });
-
-// router.get('/week7/weekseven.html', (req, res) =>{
-//     res.send(week7Page)
-// })
-
-// export default router;
